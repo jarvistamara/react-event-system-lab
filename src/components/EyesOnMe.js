ffccc37d25e4d959171ef928282105fb96fb298a
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-04-16 13:33:08 -0400

    pushing for ask a questions reps

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends Component {
+    onBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    onFocus = () => {
+        console.log('Good!')
+    }
+
+    render() {
+        return (
+            <div>
+                <button onFocus={this.onFocus} />
+                <button onBlur={this.onBlur} />
+            </div>
+        )
+    }
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,24 @@
 // Code Keypad Component Here
+import React from 'react'
+class Keypad extends Component {
+
+
+    handleInputPassword = () => {
+        console.log('Entering password...')
+    }
+
+
+    render() {
+        return (
+            <div>
+                <input type="password" onKeyUp={handleInputPassword}/>
+            </div>
+        )
+    }
+}
+export default Keypad
+
+
+
+
+
